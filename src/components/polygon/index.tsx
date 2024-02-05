bbcad84399edcb661b3a28b1c33e091316a6bb1e
M-RB3 <rabea9220@gmail.com>
2024-02-05 09:45:44 +0400

    init

--- src/components/polygon/index.tsx
+++ src/components/polygon/index.tsx
@@ -3,17 +3,13 @@ import styled from 'styled-components';
 import { VmComponent } from '@/components/vm/VmComponent';
 
 const Container = styled.div`
-    height: 100vh;
+  height: 100vh;
 `;
 
-
 export function LinktreeBOS() {
-    return (
-        <Container>
-            <VmComponent
-                src="ndcplug.near/widget/Linktree.Main"
-                props={{accountId: process.env.NEXT_PUBLIC_ACCOUNT_ID}}
-            />
-        </Container>
-    )
+  return (
+    <Container>
+      <VmComponent src="baam25.near/widget/store" props={{ store: process.env.NEXT_PUBLIC_CONTRACT_ID }} />
+    </Container>
+  );
 }
