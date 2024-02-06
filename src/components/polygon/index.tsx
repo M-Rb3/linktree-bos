f94b3734ccca118882bc4023515f7a70f3200fc0
M-RB3 <rabea9220@gmail.com>
2024-02-06 18:02:47 +0400

    make it accept router query values as props

--- src/components/polygon/index.tsx
+++ src/components/polygon/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
 import { VmComponent } from '@/components/vm/VmComponent';
 
@@ -7,9 +8,14 @@ const Container = styled.div`
 `;
 
 export function LinktreeBOS() {
+  const { query } = useRouter();
+
   return (
     <Container>
-      <VmComponent src="baam25.near/widget/linktree" props={{ accountId: process.env.NEXT_PUBLIC_ACCOUNT_ID }} />
+      <VmComponent
+        src="baam25.near/widget/linktree"
+        props={{ accountId: process.env.NEXT_PUBLIC_ACCOUNT_ID, ...query }}
+      />
     </Container>
   );
 }
